aeb4af2db64fe24cc7b40975589011086a40c18f
DiamondPurvis <purvisdiamond@gmail.com>
2019-07-22 05:57:47 -0400

    Fix get token and login

--- config/routes.js
+++ config/routes.js
@@ -1,65 +1,66 @@
-const axios = require('axios');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const tokenService = require("../auth/token-service.js");
 
-const Users = require('../config/users-model');
-const { authenticate, generateToken } = require('../auth/authenticate');
+const Users = require("./routes-model.js");
+
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
+  const hash = bcrypt.hashSync(user.password, 16);
   user.password = hash;
-
   Users.add(user)
-    .then(user => {
-      res.status(201).json(user);
+    .then(saved => {
+      const token = tokenService.generateToken(user);
+      res.status(201).json({ saved, message: `Registered, ${token}` });
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({error, message: 'Error: Cannot register user.'});
     });
 }
 
 function login(req, res) {
   // implement user login
   let { username, password } = req.body;
-
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-
+        const token = tokenService.generateToken(user);
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          token
+          message: `Welcome ${user.username}!, this is your`,
+          token,
+          roles: token.roles
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials." });
       }
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({error, message: '500 failure: Login'});
     });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
-  };
+    headers: { accept: "application/json" }
+    };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
-    .then(response => {
+  .get("https://icanhazdadjoke.com/search", requestOptions)
+  .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+      console.error(error)
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
+        });
 }
